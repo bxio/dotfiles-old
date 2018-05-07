388a90d629128895b87cf23b7f46918530ceba7e
Bill Xiong <billxiong99@gmail.com>
2018-05-07 10:30:16 -0700

    updated mac defaults

--- install.sh
+++ install.sh
@@ -28,6 +28,8 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	# Install homebrew libs & tools
 	brew install binwalk exiftool fcrackzip ffmpeg gcc gdb gettext git-lfs gist gnupg hashcat imagemagick nmap nvm pinentry pipenv python3 radare2 readline sqlite sqlmap thefuck volatility wpscan
+
+  brew cleanup
 fi
 
 
@@ -41,6 +43,8 @@ then
 
 	# Install some default apps
 	brew cask install 1password alfred bartender caffeine calibre cloudapp discord docker dropbox droplr fantastical filezilla firefox franz go2shell handbrake istat-menus iterm2 java jdownloader keybase mactex meld metasploit microsoft-office notion open-in-code rocket runescape skype slack speedcrunch steam teamviewer telegram telegram-desktop the-unarchiver torbrowser transmission vlc visual-studio-code vmware-fusion windscribe wireshark
+
+  brew cleanup
 fi
 
 if [ -d "/Applications/iTerm.app/" ]
@@ -160,34 +164,167 @@ then
 	# Ask for the administrator password upfront -- some of these commands require admin
 	sudo -v
 
-	# Require password immediately after sleep or screen saver begins
-	defaults write com.apple.screensaver askForPassword -int 1
-	defaults write com.apple.screensaver askForPasswordDelay -int 0
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
 
-	# Disable press-and-hold for keys in favor of key repeat
-	defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+  # Disable the sound effects on boot
+  sudo nvram SystemAudioVolume=" "
 
-	# Set a blazingly fast keyboard repeat rate
-	defaults write NSGlobalDomain KeyRepeat -int 2
-	defaults write NSGlobalDomain InitialKeyRepeat -int 15
+  # Always show scrollbars
+  #defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+  # Possible values: `WhenScrolling`, `Automatic` and `Always`
 
-	# Disable the “Are you sure you want to open this application?” dialog
-	defaults write com.apple.LaunchServices LSQuarantine -bool false
+  # Disable the over-the-top focus ring animation
+  defaults write NSGlobalDomain NSUseAnimatedFocusRing -bool false
 
-	# Disable “natural” (Lion-style) scrolling
-	defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+  # Expand save panel by default
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-	#Set default location for screenshots
-	defaults write com.apple.screencapture location ~/Downloads/
+  # Expand print panel by default
+  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+  defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-	# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-	defaults write com.apple.screencapture type -string "png"
+  # Save to disk (not to iCloud) by default
+  defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-	# Enable Secure Keyboard Entry in Terminal.app
-	# See: https://security.stackexchange.com/a/47786/8918
-	defaults write com.apple.terminal SecureKeyboardEntry -bool true
+  # Automatically quit printer app once the print jobs complete
+  defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-	killall SystemUIServer
+  # Disable the “Are you sure you want to open this application?” dialog
+  defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+  # Disable Resume system-wide
+  defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+  # Disable automatic termination of inactive apps
+  #defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+  # Disable the crash reporter
+  #defaults write com.apple.CrashReporter DialogType -string "none"
+
+  # Set Help Viewer windows to non-floating mode
+  defaults write com.apple.helpviewer DevMode -bool true
+
+  # Reveal IP address, hostname, OS version, etc. when clicking the clock
+  # in the login window
+  sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+  # Disable smart quotes as they’re annoying when typing code
+  defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+###############################################################################
+# SSD-specific tweaks                                                         #
+##############################################################################
+
+
+  # Disable hibernation (speeds up entering sleep mode)
+  #sudo pmset -a hibernatemode 0
+
+  # Remove the sleep image file to save disk space
+  #sudo rm /private/var/vm/sleepimage
+  # Create a zero-byte file instead…
+  #sudo touch /private/var/vm/sleepimage
+  # …and make sure it can’t be rewritten
+  #sudo chflags uchg /private/var/vm/sleepimage
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
+###############################################################################
+
+  # Trackpad: enable tap to click for this user and for the login screen
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+  defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+  defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+  # Trackpad: map bottom right corner to right-click
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+  defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+  defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+  # Disable “natural” (Lion-style) scrolling
+  defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+  # Disable press-and-hold for keys in favor of key repeat
+  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+  # Set a blazingly fast keyboard repeat rate
+  defaults write NSGlobalDomain KeyRepeat -int 1
+  defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+  # Set language and text formats
+  # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+  # `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+  defaults write NSGlobalDomain AppleLanguages -array "en-CA" "zh-Hans"
+  defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=CAD"
+  defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+  defaults write NSGlobalDomain AppleMetricUnits -bool true
+
+  # Show language menu in the top right corner of the boot screen
+  sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
+
+  # Set the timezone; see `sudo systemsetup -listtimezones` for other values
+  sudo systemsetup -settimezone "America/Vancouver" > /dev/null
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+
+  # Require password immediately after sleep or screen saver begins
+  defaults write com.apple.screensaver askForPassword -int 1
+  defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+  # Save screenshots to the desktop
+  defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+
+  # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+  defaults write com.apple.screencapture type -string "png"
+
+  # Disable shadow in screenshots
+  defaults write com.apple.screencapture disable-shadow -bool true
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+  # Finder: disable window animations and Get Info animations
+  defaults write com.apple.finder DisableAllAnimations -bool true
+  # Set Desktop as the default location for new Finder windows
+  # For other paths, use `PfLo` and `file:///full/path/here/`
+  defaults write com.apple.finder NewWindowTarget -string "PfDe"
+  defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
+
+  # Show icons for hard drives, servers, and removable media on the desktop
+  defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+  defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+  defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+  # Finder: show hidden files by default
+  #  defaults write com.apple.finder AppleShowAllFiles -bool true
+
+  # Finder: show all filename extensions
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+  # Finder: show status bar
+  defaults write com.apple.finder ShowStatusBar -bool true
+
+  # Finder: show path bar
+  defaults write com.apple.finder ShowPathbar -bool true
+
+  # When performing a search, search the current folder by default
+  defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+
+
+  # Enable Secure Keyboard Entry in Terminal.app
+  # See: https://security.stackexchange.com/a/47786/8918
+  defaults write com.apple.terminal SecureKeyboardEntry -bool true
+
+  killall SystemUIServer
 
 	# Prompt for re-login
 	echo "Some of these settings will require you to re-login..."
