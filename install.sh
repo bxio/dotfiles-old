ecf64e2f3d523e06fa34509a82344a7269cb7451
Bill Xiong <billxiong99@gmail.com>
2018-05-07 10:42:05 -0700

    fixed spacing issues

--- install.sh
+++ install.sh
@@ -167,208 +167,157 @@ then
 ###############################################################################
 # General UI/UX                                                               #
 ###############################################################################
-
-  # Disable the sound effects on boot
+# Disable the sound effects on boot
   sudo nvram SystemAudioVolume=" "
-
-  # Always show scrollbars
+# Always show scrollbars
   #defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
   # Possible values: `WhenScrolling`, `Automatic` and `Always`
-
-  # Disable the over-the-top focus ring animation
+# Disable the over-the-top focus ring animation
   defaults write NSGlobalDomain NSUseAnimatedFocusRing -bool false
-
-  # Expand save panel by default
+# Expand save panel by default
   defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
   defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-  # Expand print panel by default
+# Expand print panel by default
   defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
   defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-  # Save to disk (not to iCloud) by default
+# Save to disk (not to iCloud) by default
   defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-  # Automatically quit printer app once the print jobs complete
+# Automatically quit printer app once the print jobs complete
   defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-  # Disable the “Are you sure you want to open this application?” dialog
+# Disable the “Are you sure you want to open this application?” dialog
   defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-  # Disable Resume system-wide
+# Disable Resume system-wide
   defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
-
-  # Disable automatic termination of inactive apps
+# Disable automatic termination of inactive apps
   #defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
-
-  # Disable the crash reporter
+# Disable the crash reporter
   #defaults write com.apple.CrashReporter DialogType -string "none"
-
-  # Set Help Viewer windows to non-floating mode
+# Set Help Viewer windows to non-floating mode
   defaults write com.apple.helpviewer DevMode -bool true
-
-  # Reveal IP address, hostname, OS version, etc. when clicking the clock
-  # in the login window
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
   sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-
-  # Disable smart quotes as they’re annoying when typing code
+# Disable smart quotes as they’re annoying when typing code
   defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
 ###############################################################################
 # SSD-specific tweaks                                                         #
 ##############################################################################
 
-
-  # Disable hibernation (speeds up entering sleep mode)
+# Disable hibernation (speeds up entering sleep mode)
   #sudo pmset -a hibernatemode 0
-
-  # Remove the sleep image file to save disk space
-  #sudo rm /private/var/vm/sleepimage
-  # Create a zero-byte file instead…
-  #sudo touch /private/var/vm/sleepimage
-  # …and make sure it can’t be rewritten
+# Remove the sleep image file to save disk space
+  #sudo rm /private/var/vm/sleepimage# Create a zero-byte file instead…
+  #sudo touch /private/var/vm/sleepimage# …and make sure it can’t be rewritten
   #sudo chflags uchg /private/var/vm/sleepimage
 
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
 ###############################################################################
-
-  # Trackpad: enable tap to click for this user and for the login screen
+# Trackpad: enable tap to click for this user and for the login screen
   defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
   defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
   defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-  # Trackpad: map bottom right corner to right-click
+# Trackpad: map bottom right corner to right-click
   defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
   defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
   defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
   defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
-
-  # Disable “natural” (Lion-style) scrolling
+# Disable “natural” (Lion-style) scrolling
   defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-  # Disable press-and-hold for keys in favor of key repeat
+# Disable press-and-hold for keys in favor of key repeat
   defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-  # Set a blazingly fast keyboard repeat rate
+# Set a blazingly fast keyboard repeat rate
   defaults write NSGlobalDomain KeyRepeat -int 1
   defaults write NSGlobalDomain InitialKeyRepeat -int 10
-
-  # Set language and text formats
-  # Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
-  # `Inches`, `en_GB` with `en_US`, and `true` with `false`.
+# Set language and text formats
+# Note: if you’re in the US, replace `EUR` with `USD`, `Centimeters` with
+# `Inches`, `en_GB` with `en_US`, and `true` with `false`.
   defaults write NSGlobalDomain AppleLanguages -array "en-CA" "zh-Hans"
   defaults write NSGlobalDomain AppleLocale -string "en_GB@currency=CAD"
   defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
   defaults write NSGlobalDomain AppleMetricUnits -bool true
-
-  # Show language menu in the top right corner of the boot screen
+# Show language menu in the top right corner of the boot screen
   sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
-
-  # Set the timezone; see `sudo systemsetup -listtimezones` for other values
+# Set the timezone; see `sudo systemsetup -listtimezones` for other values
   sudo systemsetup -settimezone "America/Vancouver" > /dev/null
 
 ###############################################################################
 # Screen                                                                      #
 ###############################################################################
 
-
-  # Require password immediately after sleep or screen saver begins
+# Require password immediately after sleep or screen saver begins
   defaults write com.apple.screensaver askForPassword -int 1
   defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-  # Save screenshots to the desktop
+# Save screenshots to the desktop
   defaults write com.apple.screencapture location -string "${HOME}/Desktop"
-
-  # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
   defaults write com.apple.screencapture type -string "png"
-
-  # Disable shadow in screenshots
+# Disable shadow in screenshots
   defaults write com.apple.screencapture disable-shadow -bool true
 
 ###############################################################################
 # Finder                                                                      #
 ###############################################################################
-
-  # Finder: disable window animations and Get Info animations
+# Finder: disable window animations and Get Info animations
   defaults write com.apple.finder DisableAllAnimations -bool true
-  # Set Desktop as the default location for new Finder windows
-  # For other paths, use `PfLo` and `file:///full/path/here/`
+# Set Desktop as the default location for new Finder windows
+# For other paths, use `PfLo` and `file:///full/path/here/`
   defaults write com.apple.finder NewWindowTarget -string "PfDe"
   defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
-
-  # Show icons for hard drives, servers, and removable media on the desktop
+# Show icons for hard drives, servers, and removable media on the desktop
   defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
   defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
   defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
   defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-  # Finder: show hidden files by default
-  #  defaults write com.apple.finder AppleShowAllFiles -bool true
-
-  # Finder: show all filename extensions
+# Finder: show hidden files by default
+#  defaults write com.apple.finder AppleShowAllFiles -bool true
+# Finder: show all filename extensions
   defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-  # Finder: show status bar
+# Finder: show status bar
   defaults write com.apple.finder ShowStatusBar -bool true
-
-  # Finder: show path bar
+# Finder: show path bar
   defaults write com.apple.finder ShowPathbar -bool true
-
-  # When performing a search, search the current folder by default
+# When performing a search, search the current folder by default
   defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-  # Disable the warning when changing a file extension
+# Disable the warning when changing a file extension
   defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-  # Enable spring loading for directories
+# Enable spring loading for directories
   defaults write NSGlobalDomain com.apple.springing.enabled -bool true
-
-  # Remove the spring loading delay for directories
+# Remove the spring loading delay for directories
   defaults write NSGlobalDomain com.apple.springing.delay -float 0
-
-  # Avoid creating .DS_Store files on network or USB volumes
+# Avoid creating .DS_Store files on network or USB volumes
   defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
   defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-  # Disable disk image verification
+# Disable disk image verification
   defaults write com.apple.frameworks.diskimages skip-verify -bool true
   defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
   defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
-
-  # Automatically open a new Finder window when a volume is mounted
+# Automatically open a new Finder window when a volume is mounted
   defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
   defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
   defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
-
-  # Use list view in all Finder windows by default
-  # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
   defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-  # Disable the warning before emptying the Trash
+# Disable the warning before emptying the Trash
   defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
-  # Enable AirDrop over Ethernet and on unsupported Macs running Lion
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
   defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
-
-  # Show the ~/Library folder
+# Show the ~/Library folder
   chflags nohidden ~/Library
-
-  # Remove Dropbox’s green checkmark icons in Finder
-  # file=/Applications/Dropbox.app/Contents/Resources/emblem-dropbox-uptodate.icns
-  # [ -e "${file}" ] && mv -f "${file}" "${file}.bak"
+# Remove Dropbox’s green checkmark icons in Finder
+# file=/Applications/Dropbox.app/Contents/Resources/emblem-dropbox-uptodate.icns
+# [ -e "${file}" ] && mv -f "${file}" "${file}.bak"
 
 ###############################################################################
 # Dock, Dashboard, and hot corners                                            #
 ###############################################################################
-
-  # Enable highlight hover effect for the grid view of a stack (Dock)
+# Enable highlight hover effect for the grid view of a stack (Dock)
   defaults write com.apple.dock mouse-over-hilite-stack -bool true
-
-  # Set the icon size of Dock items to 36 pixels
+# Set the icon size of Dock items to 36 pixels
   defaults write com.apple.dock tilesize -int 36
-
-  # Change minimize/maximize window effect
+# Change minimize/maximize window effect
   defaults write com.apple.dock mineffect -string "scale"
 
   # Minimize windows into their application’s icon
@@ -596,7 +545,7 @@ then
   sudo mdutil -E / > /dev/null
 
 # Reset Launchpad, but keep the desktop wallpaper intact
-find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
+  find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
 
   # Enable Secure Keyboard Entry in Terminal.app
   # See: https://security.stackexchange.com/a/47786/8918
